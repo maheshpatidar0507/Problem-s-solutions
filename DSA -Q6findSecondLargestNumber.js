74f50570217d499cc35a6caff46eeb407d5c9e48
maheshpatidar0507 <112385935+maheshpatidar0507@users.noreply.github.com>
2023-01-10 21:25:22 +0530

    Add files via upload

--- DSA -Q6findSecondLargestNumber.js
+++ DSA -Q6findSecondLargestNumber.js
@@ -1,6 +1,6 @@
 // Question-Write a function to find the second largest number from given array.
 
-//------------------ Algorithm -----------------------------------------------------
+//----------------------------------- Algorithm -----------------------------------------------------
 //step 1- Create 2 variable,first largest and second  secondLarget and assign them 0.
 //step 1.1- Check the length of array.if length is less then or equal to 1, return "Please enter valid array".
 //-Else follow the folloing steps
@@ -11,12 +11,12 @@
 //step 4- Pick second value and follow the step 2.1, 3 and 3.1
 //step 5- At last return the secondLarget
 
-//------------code-------------------------
+//-----------------------------------------------code-------------------------------------
 function secondLargestNumber(arr) {
   let largest = 0;
   let secondLargest = 0;
   if (arr.length <= 1) {
-    console.log("Please enter valid array");
+    secondLargest = "Please enter valid array";
   } else {
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] > largest) {
@@ -29,4 +29,10 @@ function secondLargestNumber(arr) {
   return secondLargest;
 }
 console.log(secondLargestNumber([1, 6, 1, 3, 54, 23, 4]));
-// output 23
+// output =>23
+
+console.log(secondLargestNumber([25]));
+// output=> Please enter valid array
+
+console.log(secondLargestNumber([4, 12, 1, 3, -5, -8, 4]));
+// output=> 4
